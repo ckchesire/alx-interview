217f166874933328c3578e6908e93b7140b3178e
Christian Chesire <christianck.chesire@gmail.com>
2025-06-13 05:08:49 +0300

    Added solution for, determining the fewest number of coins needed to meet a given amount total.

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+"""
+Fewest number of coins needed to meet a given amount total.
+
+Args:
+  coins : number or coins
+  total : total to be met
+
+Returns:
+  (int) retuns integer representing total.
+"""
+
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
